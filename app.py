50750dbefd2d64a0fe017b53f55d9cb5929514a7
dheeru177 <128319741+dheeru177@users.noreply.github.com>
2025-05-19 14:28:44 +0530

    new

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ import joblib
 import pandas as pd
 
 app = Flask(__name__)
+
 model = joblib.load('random_forest_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input_data = request.json  # JSON input
-    df = pd.DataFrame([input_data])
-    prediction = model.predict(df)
-    return jsonify({'prediction': int(prediction[0])})
+    try:
+        input_data = request.get_json()
+        df = pd.DataFrame([input_data])
+        prediction = model.predict(df)
+        return jsonify({'prediction': int(prediction[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run()
